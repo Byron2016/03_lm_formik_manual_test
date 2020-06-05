d142956dbdc4e03e0da2f79ed4d28b70896c1238
MiPersona <bgva2005@yahoo.com>
2020-06-05 17:31:31 -0500

    0003_Formik Incorpora Primer form -- usando forma larga

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import TicToc from "./components/tic_toc";
-
+import NewsLetter from "./components/newsletter";
 
 const Root = (
   <BrowserRouter>
     <Switch>
       <Route path="/tic_toc" component={TicToc} />
-
+      <Route path="/newsletter" component={NewsLetter} />
       <Redirect from="/" to="/tic_toc" />
     </Switch>
   </BrowserRouter>
